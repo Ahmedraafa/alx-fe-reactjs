a5ad231ebb0e0e5a2bd8d4b0b34e13dc65865f6a
Ahmed Raafat <122217903+Ahmedraafa@users.noreply.github.com>
2024-08-17 18:08:30 +0300

    Styled components and fixed visibility issues

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,29 +1,30 @@
 // src/UserContext.js
 import React, { createContext, useContext } from 'react';
+
 import PropTypes from 'prop-types';
 
 // Create a Context
 const UserContext = createContext();
 
+
 // Provider component
 export function UserProvider({ children, userData }) {
-  return (
-    <UserContext.Provider value={userData}>
-      {children}
-    </UserContext.Provider>
-  );
+    return ( <
+        UserContext.Provider value = { userData } > { children } <
+        /UserContext.Provider>
+    );
 }
 
 // Prop validation for UserProvider
 UserProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-  userData: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-  }).isRequired,
+    children: PropTypes.node.isRequired,
+    userData: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+    }).isRequired,
 };
 
 // Custom hook to use UserContext
 export function useUser() {
-  return useContext(UserContext);
+    return useContext(UserContext);
 }
